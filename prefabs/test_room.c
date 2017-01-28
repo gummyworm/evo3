4c07c16aaebcdcde39fdfe21023b51b2bd22909c
Bryce Wilson <brycemwilson@gmail.com>
2017-01-28 15:07:53 -0600

    Add test room prefab

---
+++ prefabs/test_room.c
@@ -0,0 +1,9 @@
+#include "../entity.h"
+#include "../systems.h"
+
+int TestRoom(int e) {
+	AddTransform(e, 0, 0, 0);
+	AddRoom(e, "Test Room", "This is a test room", -100, -100, -100, 200,
+	        200, 200);
+	return 0;
+}
